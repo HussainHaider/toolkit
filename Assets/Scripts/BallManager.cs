1b01709f0edc5ba7712e8b2c30308a1b778d7348
Hussain Haider <hussainhaider490@gmail.com>
2018-12-17 12:11:51 +0500

    first commit

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour {
+
+	float value = 0.05f;
+	float XValue;
+	float YValue;
+	public static int script_no = 0;
+	bool collisionFlag = false;
+	float ballheightupto;
+
+	// Use this for initialization
+	void Start () {
+		ballheightupto = 5.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (script_no == 1)
+		{
+			XValue = this.transform.localPosition.x + value;
+			this.transform.localPosition = new Vector3(XValue, 0, -2.5f);
+		} else if (script_no == 2)
+		{
+			if (collisionFlag == false)
+			{
+				YValue = this.transform.localPosition.y - value;
+				this.transform.localPosition = new Vector3(0f, YValue, 0);
+
+
+			}
+			else if (collisionFlag == true)
+			{
+				YValue = this.transform.localPosition.y + value;
+				this.transform.localPosition = new Vector3(0f, YValue, 0);
+
+				if (this.transform.localPosition.y >= ballheightupto)
+				{
+					collisionFlag = false;
+				}
+			}
+		}
+
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		collisionFlag = true;
+	}
+}
---
+++ Assets/Scripts/ButtonHandlers.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandlers : MonoBehaviour {
+
+	private Button roll_btn;
+	private Button bounce_btn;
+
+	int script_no = BallManager.script_no;
+    GameObject monster;
+
+
+    void Start()
+	{
+		roll_btn = GameObject.FindGameObjectWithTag("RollBtn").GetComponent<Button>();
+		roll_btn.gameObject.SetActive(false);
+
+
+		bounce_btn = GameObject.FindGameObjectWithTag("BounceBtn").GetComponent<Button>();
+		bounce_btn.gameObject.SetActive(false);
+	}
+	public void startStimulation()
+	{
+		Debug.Log("startStimulation called.");
+        BallManager.script_no = script_no;
+    }
+	public void CreateGround()
+	{
+		Debug.Log("CreateGround called.");
+        monster = (GameObject)Instantiate(Resources.Load("Ground"));
+        monster.transform.localPosition = new Vector3(0.19f, -0.6f, -1.51f);
+    }
+	public void CreateBall()
+	{
+		Debug.Log("CreateBall called.");
+		roll_btn.gameObject.SetActive(true);
+		bounce_btn.gameObject.SetActive(true);
+	}
+	public void RollingBall()
+	{
+		Debug.Log("Rolling Ball called.");
+		script_no = 1;
+
+	}
+	public void BouncingBall()
+	{
+		Debug.Log("Bouncing Ball called.");
+		script_no = 2;
+
+	}
+}
