0a5d404f773d7b94a86698c5d1e445d7ecdc1c33
Hussain Haider <hussainhaider490@gmail.com>
2018-12-17 19:20:21 +0500

    Teacher work Completed

--- Assets/Scripts/ButtonHandlers.cs
+++ Assets/Scripts/ButtonHandlers.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Text;
 
 public class ButtonHandlers : MonoBehaviour {
 
@@ -9,12 +11,20 @@ public class ButtonHandlers : MonoBehaviour {
 	private Button bounce_btn;
 
 	int script_no = BallManager.script_no;
-    GameObject monster;
+	GameObject ground,ball;
+
+	FileStream file;
+
+    StreamWriter writer;
 
 
     void Start()
 	{
-		roll_btn = GameObject.FindGameObjectWithTag("RollBtn").GetComponent<Button>();
+
+        file = File.Open("Objects.json", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        writer = new StreamWriter(file);
+
+        roll_btn = GameObject.FindGameObjectWithTag("RollBtn").GetComponent<Button>();
 		roll_btn.gameObject.SetActive(false);
 
 
@@ -24,19 +34,43 @@ public class ButtonHandlers : MonoBehaviour {
 	public void startStimulation()
 	{
 		Debug.Log("startStimulation called.");
+
+		string script="";
+
+		if (script_no == 1)
+		{
+			script = "Rolling";
+		} else
+		{
+			script = "Bouncing";
+		}
+
+        writer.WriteLine("{ \"BallScript\": \" "+ script + "\" }");
+        writer.Close();
+
         BallManager.script_no = script_no;
-    }
+	}
 	public void CreateGround()
 	{
 		Debug.Log("CreateGround called.");
-        monster = (GameObject)Instantiate(Resources.Load("Ground"));
-        monster.transform.localPosition = new Vector3(0.19f, -0.6f, -1.51f);
-    }
+
+        writer.WriteLine("{ \"obj\": \"Ground\" }");
+        
+
+        ground = (GameObject)Instantiate(Resources.Load("Prefabs/Ground"));
+		ground.transform.localPosition = new Vector3(0.19f, -0.6f, -1.51f);
+	}
 	public void CreateBall()
 	{
 		Debug.Log("CreateBall called.");
 		roll_btn.gameObject.SetActive(true);
 		bounce_btn.gameObject.SetActive(true);
+
+        writer.WriteLine("{ \"obj\": \"Ball\" }");
+
+        ball = (GameObject)Instantiate(Resources.Load("Prefabs/Ball"));
+		ball.transform.localPosition = new Vector3(-5.76f, 0f, -2.5f);
+
 	}
 	public void RollingBall()
 	{
